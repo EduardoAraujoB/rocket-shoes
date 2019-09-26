a2b37a8b01e0261978f789f4f527a34611f483c1
unknown <ea0123450@gmail.com>
2019-09-26 17:57:07 -0300

    styling home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,95 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong> Tênis Skatista </strong>
+        <span> R$229, 99 </span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong> Tênis Skatista </strong>
+        <span> R$229, 99 </span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong> Tênis Skatista </strong>
+        <span> R$229, 99 </span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong> Tênis Skatista </strong>
+        <span> R$229, 99 </span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong> Tênis Skatista </strong>
+        <span> R$229, 99 </span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
+          alt="tenis"
+        />
+        <strong> Tênis Skatista </strong>
+        <span> R$229, 99 </span>
+        <button>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span> ADICIONAR AO CARRINHO </span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
