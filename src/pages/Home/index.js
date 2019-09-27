3eda0cb00d58f301df9b26a51d85749669a4b014
unknown <ea0123450@gmail.com>
2019-09-27 16:28:57 -0300

    searching products from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong> Tênis Skatista </strong>
-        <span> R$229, 99 </span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong> Tênis Skatista </strong>
-        <span> R$229, 99 </span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong> Tênis Skatista </strong>
-        <span> R$229, 99 </span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong> Tênis Skatista </strong>
-        <span> R$229, 99 </span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong> Tênis Skatista </strong>
-        <span> R$229, 99 </span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-nike-sb-check-solar-cnvs-masculino/56/D12-2759-256/D12-2759-256_zoom1.jpg"
-          alt="tenis"
-        />
-        <strong> Tênis Skatista </strong>
-        <span> R$229, 99 </span>
-        <button>
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span> ADICIONAR AO CARRINHO </span>
-        </button>
-      </li>
-    </ProductList>
-  );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormated: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data });
+  };
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormated}</span>
+            <button>
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+              <span> ADICIONAR AO CARRINHO </span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
+
+export default Home;
