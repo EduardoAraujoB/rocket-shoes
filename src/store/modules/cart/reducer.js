375f57dcd11cc4b32bc04ee69a21b44f2846d08d
unknown <ea0123450@gmail.com>
2019-09-29 13:13:45 -0300

    fixed bug of duplicate products in the cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,16 @@
+import produce from 'immer';
+
 const cart = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, { ...action.product, ammount: 1 }];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        if (productIndex >= 0) {
+          draft[productIndex].ammount += 1;
+        } else {
+          draft.push({ ...action.product, ammount: 1 });
+        }
+      });
     default:
       return state;
   }
